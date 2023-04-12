548eec7481b0bf7722226e3d03090a983fd20a19
thinhloi <19520691@gm.uit.edu.vn>
2023-04-12 16:52:06 +0700

    add button component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+import Button from './components/Buttons'
+import * as React from 'react';
+import { MdAddShoppingCart } from "react-icons/md";
+import Btn from './components/Btn'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Button>Default</Button>
+    <br/>
+    <br/>
+    <Button variant ='outline' >Default</Button>
+    <br/>
+    <br/>
+    <Button variant ='text' >Default</Button>
+    <br/>
+    <br/>
+    <Button disabled >Disable</Button>
+    <br/>
+    <br/>
+    <Button 
+      variant ='text' disabled> 
+      Disable
+    </Button>
+    <br/>
+    <br/>
+    <Button variant = 'iconButton' startIcon ={<MdAddShoppingCart />}>
+      &emsp;Default      
+    </Button>
+    <br/>
+    <br/>
+    <Button variant = 'iconButton' endIcon ={<MdAddShoppingCart />}> Default&emsp; </Button>
+    <br/>
+    <br/>
+    <Btn size = "sm">Default</Btn>
+    <Btn size = "md">Default</Btn>
+    <Btn size = "lg">Default</Btn>
+  <br/>
+  <br/>
+  <br/>
+    <Btn color ="default">Default</Btn>
+    <Btn color ="primary">Default</Btn>
+    <Btn color ="secondary">Default</Btn>
+    <Btn color ="danger">Default</Btn>
+
     </div>
   );
 }
